9c9272fdd2bbe05fc3b29a174578d1eec40427f1
Henrique Franco <henriqfvr@gmail.com>
2024-09-15 15:32:29 -0300

    edits

--- js/games.js
+++
@@ -1,6 +0,0 @@
-const toggleGames = document.getElementById('toggleGames');
-const games = document.getElementById('games');
-
-toggleGames.addEventListener('click', () => {
-    games.classList.toggle("opened")
-})
---
+++ js/montagem.js
@@ -0,0 +1,5 @@
+const montagemButton = document.getElementById('montagem');
+
+montagemButton.addEventListener('click', () => {
+    window.location.href = 'montagem.html'
+})

bdaf55a0d18d6f3f69c650ba50c2806c187a9215
JoaodeAlcantara <joao.gab0509@gmail.com>
2024-10-16 17:22:07 -0300

    threeDotsTab add

--- js/header.js
+++ js/header.js
@@ -3,11 +3,41 @@ const verticalNav = document.querySelector('.verticalNav');
 
 verticalNav.style.display = 'none'
 
+const threeDotsTab = document.querySelector('.threeDotsTab');
+
+threeDotsTab.style.display = 'none';
+
 toggleMenu.addEventListener('click', () => {
-    if(verticalNav.style.display === 'none'){
+    if (verticalNav.style.display === 'none') {
         verticalNav.style.display = 'block';
+
+        if(threeDotsTab.style.display === 'block'){
+            threeDotsTab.style.display = 'none'
+        }
     }
-    else{
+    else {
         verticalNav.style.display = 'none';
     }
 })
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1200) {
+        verticalNav.style.display = 'none';
+    }
+});
+
+// tres pontos aba:
+
+const tresPontos = document.querySelectorAll('.tres').forEach((el) => {
+    el.addEventListener('click', () => {
+        if (threeDotsTab.style.display === 'none') {
+            threeDotsTab.style.display = 'block';
+
+            if (verticalNav.style.display === 'block') {
+                verticalNav.style.display = 'none'
+            }
+        } else {
+            threeDotsTab.style.display = 'none'
+        }
+    })
+});

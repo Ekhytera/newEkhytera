a28b953b3187ca8519d6826a1447eef9192345fb
Henrique Franco <henriqfvr@gmail.com>
2024-10-24 19:48:58 -0300

    todos os jogos adicionados a página de montagem

--- js/funcionalidade.js
+++ js/funcionalidade.js
@@ -125,6 +125,206 @@ const games = [
             RAM: 8,
         },
     },
+    {
+        name: 'hollowknight',
+        minHardware: {
+            GPU: {
+                model: 'GeForce 9800GTX',
+                rating: 52,
+            },
+            CPU: {
+                model: 'Pentium E5200',
+                rating: 51,
+            },
+            RAM: 4,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GeForce GTX 560',
+                rating: 53,
+            },
+            CPU: {
+                model: 'Intel Core i5 750',
+                rating: 54,
+            },
+            RAM: 8,
+        },
+    },
+    {
+        name: 'minecraft',
+        minHardware: {
+            GPU: {
+                model: 'Intel HD Graphics 4000',
+                rating: 51,
+            },
+            CPU: {
+                model: 'Intel Core i3-3210',
+                rating: 51,
+            },
+            RAM: 2,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GeForce GTX 750',
+                rating: 55,
+            },
+            CPU: {
+                model: 'Intel Core i5-4690',
+                rating: 54,
+            },
+            RAM: 4,
+        },
+    },
+    {
+        name: 'nfs',
+        minHardware: {
+            GPU: {
+                model: 'GeForce GTX 760',
+                rating: 55,
+            },
+            CPU: {
+                model: 'Core i5-3570',
+                rating: 57,
+            },
+            RAM: 8,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GeForce GTX 1060',
+                rating: 66,
+            },
+            CPU: {
+                model: 'Core i7-4790',
+                rating: 61,
+            },
+            RAM: 4,
+        },
+    },
+    {
+        name: 'rdr2',
+        minHardware: {
+            GPU: {
+                model: 'GTX 770',
+                rating: 56,
+            },
+            CPU: {
+                model: 'Core i5-2500k',
+                rating: 76,
+            },
+            RAM: 8,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GeForce GTX 1060',
+                rating: 66,
+            },
+            CPU: {
+                model: 'Core i7-4770',
+                rating: 56,
+            },
+            RAM: 12,
+        },
+    },
+    {
+        name: 'rdr2',
+        minHardware: {
+            GPU: {
+                model: 'GTX 770',
+                rating: 56,
+            },
+            CPU: {
+                model: 'Core i5-2500k',
+                rating: 76,
+            },
+            RAM: 8,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GeForce GTX 1060',
+                rating: 66,
+            },
+            CPU: {
+                model: 'Core i7-4770',
+                rating: 56,
+            },
+            RAM: 12,
+        },
+    },
+    {
+        name: 'roblox',
+        minHardware: {
+            GPU: {
+                model: 'AMD Radeon 9500',
+                rating: 50,
+            },
+            CPU: {
+                model: 'Intel Celeron 430',
+                rating: 50,
+            },
+            RAM: 1,
+        },
+        recHardware: {
+            GPU: {
+                model: 'AMD Radeon R7 240',
+                rating: 54,
+            },
+            CPU: {
+                model: 'Core i3-2120',
+                rating: 53,
+            },
+            RAM: 4,
+        },
+    },
+    {
+        name: 'rleague',
+        minHardware: {
+            GPU: {
+                model: 'GeForce GTX 760',
+                rating: 55,
+            },
+            CPU: {
+                model: 'Pentium E5200',
+                rating: 51,
+            },
+            RAM: 4,
+        },
+        recHardware: {
+            GPU: {
+                model: 'Geforce GTX 1060',
+                rating: 66,
+            },
+            CPU: {
+                model: 'Core i3-3330',
+                rating: 54,
+            },
+            RAM: 8,
+        },
+    },
+    {
+        name: 'thesims4',
+        minHardware: {
+            GPU: {
+                model: 'GeForce 6600',
+                rating: 50,
+            },
+            CPU: {
+                model: 'Intel Core i3-3220',
+                rating: 53,
+            },
+            RAM: 4,
+        },
+        recHardware: {
+            GPU: {
+                model: 'GTX 650',
+                rating: 53,
+            },
+            CPU: {
+                model: 'Core i3-3330',
+                rating: 54,
+            },
+            RAM: 8,
+        },
+    },
 ];
 
 const fortnite = games.find(game => game.name === 'fortnite');
@@ -132,12 +332,26 @@ const valorant = games.find(game => game.name === 'valorant');
 const cs2 = games.find(game => game.name === 'cs2');
 const lol = games.find(game => game.name === 'lol');
 const gtaV = games.find(game => game.name === 'gtaV');
+const hknight = games.find(game => game.name === 'hollowknight');
+const minecraft = games.find(game => game.name === 'minecraft');
+const nfs = games.find(game => game.name === 'nfs');
+const rdr2 = games.find(game => game.name === 'rdr2');
+const roblox = games.find(game => game.name === 'roblox');
+const rleague = games.find(game => game.name === 'rleague');
+const thesims4 = games.find(game => game.name === 'thesims4');
 
 let fortniteButton = document.getElementById('fort');
 let valorantButton = document.getElementById('val');
 let cs2Button = document.getElementById('cs');
 let lolButton = document.getElementById('league');
 let gtaButton = document.getElementById('gta');
+let hkButton = document.getElementById('hknight');
+let mineButton = document.getElementById('minecraft');
+let nfsButton = document.getElementById('nfs');
+let rdr2Button = document.getElementById('rdr2');
+let robloxButton = document.getElementById('roblox');
+let rleagueButton = document.getElementById('rleague');
+let theSims4Button = document.getElementById('thesims4');
 
 let gameOptionsDiv = document.getElementById('gameOptions');
 const printGPU = document.getElementById('gpu');
@@ -149,7 +363,7 @@ let selectedGames = [];
 function selectionToggler(button, game) {
     if (button.style.textDecoration === '') {
         button.style.textDecoration = 'line-through';
-        button.style.opacity = 0.55;
+        button.style.opacity = 0.5;
         selectedGames.push(game);
     }
     else {
@@ -182,6 +396,33 @@ gtaButton.addEventListener('click', function () {
     selectionToggler(gtaButton, gtaV);
 });
 
+hkButton.addEventListener('click', function () {
+    selectionToggler(hkButton, hknight);
+});
+
+mineButton.addEventListener('click', function () {
+    selectionToggler(mineButton, minecraft);
+});
+
+nfsButton.addEventListener('click', function () {
+    selectionToggler(nfsButton, nfs);
+});
+
+rdr2Button.addEventListener('click', function () {
+    selectionToggler(rdr2Button, rdr2);
+});
+
+robloxButton.addEventListener('click', function () {
+    selectionToggler(robloxButton, roblox);
+});
+
+rleagueButton.addEventListener('click', function () {
+    selectionToggler(rleagueButton, rleague);
+});
+
+theSims4Button.addEventListener('click', function () {
+    selectionToggler(theSims4Button, thesims4);
+});
 
 function calculateRecSpecsPC() {
     if (selectedGames.length === 0) {

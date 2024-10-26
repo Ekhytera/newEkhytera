bc6b8fd2897cab779c53142504700fa8f7321723
Henrique Franco <henriqfvr@gmail.com>
2024-10-26 16:00:31 -0300

    botão catálogo animação

--- js/buttonAnimation.js
+++ js/buttonAnimation.js
@@ -22,3 +22,23 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error("Error");
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('animatedButton2');
+    if (button) {
+        let hue = 200;
+        let saturation = 50;
+        let lightness = 70;
+        let direction = 1;
+        function animateGradient() {
+            lightness += 0.2 * direction;
+            if (lightness >= 85 || lightness <= 55) direction *= -1;
+            button.style.setProperty('--myColor1', `hsl(${hue}, ${saturation}%, ${lightness}%)`);
+            button.style.setProperty('--myColor2', `hsl(${hue}, ${saturation}%, ${lightness - 10}%)`);
+            requestAnimationFrame(animateGradient);
+        }
+        animateGradient();
+    } else {
+        console.error("Error");
+    }
+});

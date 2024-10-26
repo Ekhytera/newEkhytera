e7758239ddbb116eb257b4d2a168195f2795ad6d
Henrique Franco <henriqfvr@gmail.com>
2024-10-26 15:26:48 -0300

    button animation added

---
+++ js/buttonAnimation.js
@@ -0,0 +1,31 @@
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function (callback) {
+        return setTimeout(callback, 1000 / 60);
+    };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('animatedButton');
+
+    if (button) {
+        let hue = 200;
+        let saturation = 50;
+        let lightness = 70;
+        let direction = 1;
+
+        function animateGradient() {
+            lightness += 0.2 * direction;
+
+            if (lightness >= 85 || lightness <= 55) direction *= -1;
+
+            button.style.setProperty('--myColor1', `hsl(${hue}, ${saturation}%, ${lightness}%)`);
+            button.style.setProperty('--myColor2', `hsl(${hue}, ${saturation}%, ${lightness - 10}%)`);
+
+            requestAnimationFrame(animateGradient);
+        }
+
+        animateGradient();
+    } else {
+        console.error("Error");
+    }
+});
--- js/montagem.js
+++
@@ -1,5 +0,0 @@
-const montagemButton = document.getElementById('montagem');
-
-montagemButton.addEventListener('click', () => {
-    window.location.href = 'montagem.html'
-})

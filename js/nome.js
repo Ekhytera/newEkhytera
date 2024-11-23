7b41d92634fc384fa3ccffc4f5d89badd4bbbbab
JoaodeAlcantara <jgateixeira02@gmail.com>
2024-11-23 13:09:37 -0300

    verificação para o tamanho do nome e email do usuario adicionada na home e no menu

--- js/nome.js
+++ js/nome.js
@@ -10,13 +10,32 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (userAtual) {
         document.querySelectorAll('#nomeUser').forEach(function (element) {
-            element.innerHTML = userAtual.toUpperCase() + ', ';
+            if(userAtual.length>10){
+                if(userAtual.includes(' ')){
+                    let primeiroNome = userAtual.split(' ')[0]
+                    element.innerHTML = primeiroNome.toUpperCase()+', '
+                } else{
+                    element.innerHTML = userAtual.slice(0,10).toUpperCase+'...'+', ';
+                }
+            } else {
+                element.innerHTML = userAtual.toUpperCase() + ', ';
+            }
+
         });
         document.querySelectorAll('#userName').forEach(function (element) {
-            element.innerHTML = userAtual;
+            if(userAtual.length>10){
+                element.innerHTML = userAtual.slice(0,10)+'...';
+            } else {
+                element.innerHTML = userAtual;
+            }
         })
         document.querySelectorAll('#CadatroInfo').forEach(function (element) {
-            element.innerHTML = userAtualEmail;
+            if(userAtualEmail.length>10){
+                element.innerHTML = userAtualEmail.slice(0,10)+'...';
+                console.log(userAtualEmail.length)
+            } else{
+                element.innerHTML = userAtualEmail;
+            }
         })
 
         infoConta.innerHTML = '<h3>Minha conta</h3> <p id="leave">Sair</p>'

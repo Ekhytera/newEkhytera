711f29abb78de8e1a6cee05d3cd5f98576b4a448
JoaodeAlcantara <jgateixeira02@gmail.com>
2024-11-25 11:16:04 -0300

    estilo no menu alterado para delimitar o tamanho do nome e email do usuario que ira aparecer

--- js/nome.js
+++ js/nome.js
@@ -11,31 +11,17 @@ window.addEventListener('DOMContentLoaded', () => {
     if (userAtual) {
         document.querySelectorAll('#nomeUser').forEach(function (element) {
             if(userAtual.length>10){
-                if(userAtual.includes(' ')){
-                    let primeiroNome = userAtual.split(' ')[0]
-                    element.innerHTML = primeiroNome.toUpperCase()+', '
-                } else{
-                    element.innerHTML = userAtual.slice(0,10).toUpperCase+'...'+', ';
-                }
+                element.innerHTML = userAtual.slice(0, 10).toUpperCase()+'...' + ', ';
             } else {
                 element.innerHTML = userAtual.toUpperCase() + ', ';
             }
 
         });
         document.querySelectorAll('#userName').forEach(function (element) {
-            if(userAtual.length>10){
-                element.innerHTML = userAtual.slice(0,10)+'...';
-            } else {
                 element.innerHTML = userAtual;
-            }
         })
         document.querySelectorAll('#CadatroInfo').forEach(function (element) {
-            if(userAtualEmail.length>10){
-                element.innerHTML = userAtualEmail.slice(0,10)+'...';
-                console.log(userAtualEmail.length)
-            } else{
                 element.innerHTML = userAtualEmail;
-            }
         })
 
         infoConta.innerHTML = '<h3>Minha conta</h3> <p id="leave">Sair</p>'
